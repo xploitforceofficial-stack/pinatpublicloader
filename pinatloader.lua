bb517f4ae113e7e60835b2e5485c07f58c99b625
Vinzee <xploitforceofficial@gmail.com>
2026-04-12 13:06:54 +0700

    Update pinatloader.lua

--- pinatloader.lua
+++ pinatloader.lua
@@ -118,11 +118,10 @@ local _0x_v1 = {
     [10449761463] = "https://raw.githubusercontent.com/xploitforceofficial-stack/pinatpublic/refs/heads/main/public.lua", -- The Strongest Battlegrounds
     [98502499119821] = "https://raw.githubusercontent.com/xploitforceofficial-stack/pinatheavy3/refs/heads/main/3.lua", -- Heavyweight Fishing
     [116139828947259] = "https://gitlua.tuffgv.my.id/raw/w-7", -- Survive the Apocalypse
-    [13772394625] = "https://pinatbladeball-peotect.vercel.app/api/script.js", -- Blade Ball Normal Server
-    [15234596844] = "https://pinatbladeball-peotect.vercel.app/api/script.js", -- Blade Ball Training Server
-    [15144787112] = "https://pinatbladeball-peotect.vercel.app/api/script.js", -- Blade Ball Duel Server
+    [13772394625] = "https://pinatbladeball-peotect.vercel.app/api/script.js", -- Blade Ball
     [124473577469410] = "https://gitlua.tuffgv.my.id/raw/www-2" -- Be a Lucky Block
 }
+
 local _0x_v2 = game.PlaceId
 local _0x_v3 = _0x_v1[_0x_v2]
 
